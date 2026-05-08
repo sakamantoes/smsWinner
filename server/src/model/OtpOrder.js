413ac74a65f20efb55c10041ab00c8f6ab94dd30
leoTechHub <godswilltrade@gmail.com>
2026-05-08 23:35:05 +0100

    adding number

---
+++ server/src/model/OtpOrder.js
@@ -0,0 +1,79 @@
+// models/OtpOrder.js
+
+import mongoose from "mongoose";
+
+const otpOrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    provider: {
+      type: String,
+      enum: ["nodeotp", "smsactivate"],
+      required: true,
+    },
+
+    orderId: {
+      type: String,
+      required: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    service: {
+      type: String,
+      required: true,
+    },
+
+    country: {
+      type: Number,
+      required: true,
+    },
+
+    operator: {
+      type: String,
+      default: "any",
+    },
+
+    otpCode: {
+      type: String,
+      default: null,
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "PENDING",
+        "WAITING_FOR_SMS",
+        "OTP_RECEIVED",
+        "COMPLETED",
+        "CANCELLED",
+        "FAILED",
+      ],
+      default: "PENDING",
+    },
+
+    cost: {
+      type: Number,
+      default: 0,
+    },
+
+    rawResponse: {
+      type: Object,
+      default: {},
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model(
+  "OtpOrder",
+  otpOrderSchema
+);
