66e8f91fc244db79eb490b552baf01b904b58f2b
Plug-S <140978960+sakamantoes@users.noreply.github.com>
2026-05-08 06:49:32 +0100

    Refactor logSchema to use array format

--- server/src/validator/auth.validator.js
+++ server/src/validator/auth.validator.js
@@ -17,13 +17,22 @@ const loginSchema = [
 
 const googleSchema = [body("token").notEmpty().withMessage("missing token")];
 
-const logSchema = {
-  email: body("email").isEmail().withMessage("Valid email is required"),
-  password: body("password")
+const logSchema = [
+  body("email")
+    .isEmail()
+    .withMessage("Valid email is required"),
+
+  body("password")
     .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters long"),
-  price: body("price").isFloat({ min: 0 }).withMessage("Invalid price"),
-  country: body("country").notEmpty().withMessage("Country is required"),
-};
+
+  body("price")
+    .isFloat({ min: 0 })
+    .withMessage("Invalid price"),
+
+  body("country")
+    .notEmpty()
+    .withMessage("Country is required"),
+];
 
 export { registerSchema, loginSchema, googleSchema, logSchema };
