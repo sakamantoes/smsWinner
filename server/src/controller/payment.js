2a47f177fad70f93e11d432ea95a15fb1be7f868
Joel Ababio Dartey <joeldartey71@gmail.com>
2026-05-10 08:57:52 +0100

    samedashboard

--- server/src/controller/payment.js
+++ server/src/controller/payment.js
@@ -1,8 +1,18 @@
+import axios from "axios";
+
 const initialiseDeposite = async (req, res, next) => {
-  const {} = req.body;
-  const user = req.user
+  const { amount } = req.body;
+  const user = req.user;
   try {
-    
+    const pay = axios.post(
+      "https://apibox.alatpay.ng/merchant-onboarding/api/v1/payment/initialize",
+      {
+        email: user.email,
+        redirectUrl: "http://localhost:5173/",
+        amount,
+        currency: 2,
+      },
+    );
   } catch (error) {
     next(error);
   }
