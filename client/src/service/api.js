05fd927cffb08bbee5a4e756e6d1dee9d8042aef
leoTechHub <godswilltrade@gmail.com>
2026-05-18 10:38:25 +0100

    hfjjfhksksjirn

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL_LOCAL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
   headers:{
      "Content-Type": "application/json",
