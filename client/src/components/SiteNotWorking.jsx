aaa7c29c43e14fd6cdda5004e5a3f7312af517e5
Joel Ababio Dartey <joeldartey71@gmail.com>
2026-05-20 16:42:20 +0100

    site_head_shaking

---
+++ client/src/components/SiteNotWorking.jsx
@@ -0,0 +1,7 @@
+export default function SiteNotWorking() {
+  return (
+    <div className="overflow-hidden">
+      <div className="bg-white w-full h-screen animate-spin"></div>
+    </div>
+  );
+}
